97d6053db7b5d95dea3247af18efb434a9f8028a
Migolo <miguelmateusmf@gmail.com>
2023-08-23 21:25:30 +0100

    Added .map rendering and welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+import React from "react";
+
 export default function Home() {
+  const [nome, setNome] = React.useState("utilizador");
+  const [taskList, setTaskList] = React.useState([
+    { id: "1", name: "clean kitchen" },
+    { id: "2", name: "buy lightbulbs" },
+  ]);
+
   return (
     <main>
       <h1 className="text-center">Task Tracker</h1>
+      <h2>
+        Olá{" "}
+        <input
+          placeholder={nome}
+          onChange={(e) => setNome(e.target.value)}
+        ></input>
+        , tens x tarefas por concluir.
+      </h2>
+      <ul>
+        {taskList.map((task) => (
+          <li key={task.id}>{task.name}</li>
+        ))}
+      </ul>
     </main>
   );
 }
