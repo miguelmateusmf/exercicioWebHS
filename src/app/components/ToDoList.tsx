67436fc572fef4ac00b38b5434850f312d1c97ab
Migolo <miguelmateusmf@gmail.com>
2023-08-27 17:30:26 +0100

    added function to count how many days are left to complete the task

--- src/app/components/ToDoList.tsx
+++ src/app/components/ToDoList.tsx
@@ -2,13 +2,25 @@
 import React from "react";
 import Modal from "./Modal";
 
+interface Task {
+  id: number;
+  name: string;
+  date: string;
+  completed: boolean;
+}
+
 export default function Home() {
   const [taskList, setTaskList] = React.useState([
     { id: 1.1, name: "clean kitchen", date: "", completed: false },
     { id: 2.1, name: "buy lightbulbs", date: "", completed: true },
   ]);
+  const [taskCom, setTaskCom] = React.useState(true);
   const [tempName, setTempName] = React.useState("");
   const [tempDate, setTempDate] = React.useState("");
+  const [listType, setListType] = React.useState("isCompleted");
+
+  const isCompleted = (task: Task) => task.completed === true;
+  const isNotCompleted = (task: Task) => task.completed === false;
 
   function handleSubmit(): void {
     let copy = [...taskList];
@@ -57,6 +69,23 @@ export default function Home() {
     setTaskList(updatedTasks);
   }
 
+  function List() {}
+
+  function calculateDaysBetweenDates(date: string): number {
+    const date3 = new Date();
+    const date2 = new Date(
+      date3.getFullYear(),
+      date3.getMonth(),
+      date3.getDate()
+    );
+    const date1 = new Date(date);
+    const timeDifference = Math.abs(date2.getTime() - date1.getTime());
+
+    const daysDifference = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
+
+    return daysDifference;
+  }
+
   return (
     <div>
       <input
@@ -77,12 +106,12 @@ export default function Home() {
       >
         Add Task
       </button>
-
       <ul>
         {taskList.map((task, index) => (
           <li key={index}>
             <div className={`${task.completed === true ? "line-through" : ""}`}>
-              {task.name} {task.date} {task.id}
+              {task.name} {task.date} {task.id} ...
+              {calculateDaysBetweenDates(task.date)}
             </div>
             <button onClick={(e) => removeTask(index)}>delete</button>...
             <button onClick={(e) => toogleCompleted(task.id)}>toogle</button>...
@@ -94,6 +123,27 @@ export default function Home() {
             />
           </li>
         ))}
+        asdasdasda
+        {taskList
+          .filter((task) => task.completed === true || false)
+          .map((task, index) => (
+            <li key={index}>
+              <div
+                className={`${task.completed === true ? "line-through" : ""}`}
+              >
+                {task.name} {task.date} {task.id}
+              </div>
+              <button onClick={(e) => removeTask(index)}>delete</button>...
+              <button onClick={(e) => toogleCompleted(task.id)}>toogle</button>
+              ...
+              <Modal
+                id={task.id}
+                name={task.name}
+                date={task.date}
+                editTask={editTask}
+              />
+            </li>
+          ))}
       </ul>
       {tempName}
       {tempDate.toString()}
